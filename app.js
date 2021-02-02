95729161586ef7aa4681a244a40ef43098914983
Popoy Vargas <popoyvargas@me.com>
2021-02-02 17:12:42 +0800

    Add cors middleware.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const { invalidRoutesHandler, globalErrorHandler } = require('zv-express-error-handler');
 
 const photosRouter = require('./routes/photosRoutes');
@@ -7,6 +8,7 @@ const photosRouter = require('./routes/photosRoutes');
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
